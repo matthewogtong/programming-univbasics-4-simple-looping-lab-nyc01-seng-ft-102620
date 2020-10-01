0b74622578fbbc797541a2d22279c36a23086475
Matthew Ogtong <ogtongm@gmail.com>
2020-10-01 01:59:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,3 +8,14 @@ def loop_message_five_times(message)
     counter += 1
   end
 end
+
+
+def loop_message_n_times(message, number)
+
+  counter = 0
+
+  while counter < number do
+    puts message
+    counter += 1
+  end
+end
